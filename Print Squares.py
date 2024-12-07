d16c4fea4a0be22325a226892b1b1c4dfa1f8b86
Md. Jahidul Islam <70716785+jahid-github@users.noreply.github.com>
2024-12-07 18:43:18 +0200

    Add files via upload

---
+++ Print Squares.py
@@ -0,0 +1,18 @@
+"""Print Squares
+Write a program to output the squares (using multiplication) of numbers from 1 to 5 on separate lines.
+
+[Note: Please print in the same format as given below. There are single spaces between hyphen(-) and digits.]
+
+Output Format
+1 - 1
+2 - 4
+3 - 9
+4 - 16
+5 - 25
+"""
+a=1**2
+b=2**2
+c=3**2
+d=4**2
+e=5**2
+print(f"1 - {a}\n2 - {b}\n3 - {c}\n4 - {d}\n5 - {e}")
